760e60fe84248c09085248dc182aaf4213593850
nima.mirzaei@hotmail.com <nima.mirzaei@hotmail.com>
2020-10-25 23:56:54 -0400

    group component display, function, and conditional rendering added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,33 @@ import './App.css';
 import { useState } from 'react';
 
 
-const Groups = () => {
+function Groups() {
   const [showGroupForm, setShowGroupForm] = useState(false);
-  
+  const [selectedGroup, setSelectedGroup] = useState(null);
+  const [showTasks, setShowTasks] = useState(false);
+
+  const viewGroup = (group) => {
+    setSelectedGroup(group);
+    setShowTasks(true);
+  }
+
   const removeGroup = (group) => localStorage.removeItem(group);
 
   return (
     <div className='groups'>
       <button onClick={setShowGroupForm(true)}>+</button>
+      <ul>
+        {Object.keys(localStorage).map((group) => {
+          return (
+            <li key={group}>
+              <button onClick={() => removeGroup.bind(group)}>x</button>
+              <p onClick={() => viewGroup.bind(group)}>{group.name}</p>
+            </li>
+          )
+        })}
+      </ul>
+      { showGroupForm && <GroupForm /> }
+      { showTasks && <Tasks group={selectedGroup}/> }
     </div>
   )
 }
